160552f307e2665dd726609a340ca679b6750b25
artist0123 <napatart1111@gmail.com>
2023-05-01 00:04:20 +0700

    commit

--- dynamoDBClient.js
+++ dynamoDBClient.js
@@ -1,13 +1,13 @@
+require("dotenv").config();
 const ddbClient = require("@aws-sdk/lib-dynamodb");
 const AWS = require("@aws-sdk/client-dynamodb");
 
 const client = new AWS.DynamoDBClient({
   region: "us-east-1",
   credentials: {
-    accessKeyId: "ASIAX4DTSJABMGXOLOCT", // ใส่ Access Key ID ที่มีสิทธิ์เข้าถึง DynamoDB
-    secretAccessKey: "QGnBr3f9XvBOw38aoLUguFmAFF7rnPTm4neQnBKh", // ใส่ Secret Access Key ที่มีสิทธิ์เข้าถึง DynamoDB
-    sessionToken:
-      "FwoGZXIvYXdzEIH//////////wEaDDmjmnl4blmOzkqgyyLIAZCTQAXC+P/gfdDhq1LUqMW8W3LKlb/Jh2RO0ibl/cYdS2Kx1zkOTuHUwf2imgacJqdq5NNmXH7lHC7UCMkKUexwJgHkG7xmDojLzq9YZ2JnJQuIlSOrSwc/N96xUWNv5/qY6HFQ8P3UwIeCJ33s7hdlZ6520tL650tbkauJJMz8fexyWWVP2/zv/E+/7htLcmG5UPFUatkz4i33GvX8UjfJ13CGqqr+QBnHNFRFAtURYlgp1n1xCSBhQJWEBUjOdQHVk+RhzOEyKMf7tKIGMi2L4gfBjClOURNWAnZdAngC8R0NvHiA5d7td5PfXs6fKGDTZlmw3MGsDr2DnR0=",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN,
   },
 });
 const marshallOptions = {
@@ -30,5 +30,3 @@ const documentClient = ddbClient.DynamoDBDocumentClient.from(
 );
 module.exports = documentClient;
 
-//AKIAUPBD4PZLMAKGRZTG
-//zbNFqdC87+5CD15jjvSgIG+47CkoF9TCu8iTAZ+j
